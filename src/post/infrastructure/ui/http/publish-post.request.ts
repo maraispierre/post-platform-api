100e6c5aa55ab1711873fc7353e64f9ac722faa7
Marais <piemarais@gmail.com>
2021-05-05 21:20:46 +0200

    Add size control on publish post

--- src/post/infrastructure/ui/http/publish-post.request.ts
+++ src/post/infrastructure/ui/http/publish-post.request.ts
@@ -1,6 +1,7 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, Length } from 'class-validator';
 
 export class PublishPostRequest {
   @IsNotEmpty()
+  @Length(1, 80)
   public readonly content: string;
 }
